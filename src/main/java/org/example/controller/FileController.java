b9a4aae718039812d7f35831354308f29202d9a6
Pasquale <pasqualemolinaro97@gmail.com>
2022-12-20 17:47:10 +0100

    v 0.3

--- src/main/java/org/example/controller/FileController.java
+++ src/main/java/org/example/controller/FileController.java
@@ -50,4 +50,48 @@ public class FileController
             return new ResponseEntity("FIle non esistente", HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("/filePerTipo")
+    public ResponseEntity<List<File>> getFilePerTitolo(@RequestParam String string)
+    {   try
+        {
+            return new ResponseEntity(fileService.trovaFilePerTitolo(string), HttpStatus.OK);
+        }catch (RuntimeException e)
+        {
+            return new ResponseEntity("FIle non esistente", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/aggiungiFile")
+    public ResponseEntity<String> aggiungiFile(@RequestBody File file)
+    {   try
+        {   fileService.aggiungiFile(file);
+            return new ResponseEntity("File aggiunto", HttpStatus.OK);
+        }catch(RuntimeException e)
+        {
+            return  new ResponseEntity("Qualcosa è andato storto", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PostMapping("/aggiungiPreferito")
+    public ResponseEntity<String> aggiungiPreferito(@RequestBody File file, @RequestBody User user)
+    {   try
+        {   fileService.aggiungiPreferito(user, file);
+            return new ResponseEntity<String>("File aggiunto con successo ai preferiti", HttpStatus.OK);
+        }catch(RuntimeException e)
+        {   return new ResponseEntity<String>("File non aggiunto", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/rimuoviPreferito")
+    public ResponseEntity<String> rimuoviPreferito(@RequestBody File file, @RequestBody User user)
+    {   try
+        {   fileService.rimuoviPreferito(user, file);
+            return new ResponseEntity<String>("FIle rimosso con successo", HttpStatus.OK);
+        }catch(RuntimeException e)
+        {
+            return new ResponseEntity<String>("Qualcosa è andato storto", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
